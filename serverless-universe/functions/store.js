8fe574b53aeccd482d9cdf3a4e5956e0b2701d60
Ulises Gascon <ulises.gascon@guidesmiths.com>
2020-02-08 13:06:23 +0100

    All the steps done!

--- serverless-universe/functions/store.js
+++ serverless-universe/functions/store.js
@@ -4,3 +4,30 @@ const admin = require('firebase-admin')
 admin.initializeApp(functions.config().firebase)
 
 const db = admin.firestore();
+const relaysCollection = db.collection("relays")
+
+const addRelay = (relay) => {
+  console.log(`Adding relay ${relay.fingerprint} (${relay.nickname})...`)
+  return relaysCollection.doc(relay.fingerprint).set(relay)
+}
+
+const getAllRelays = async () => {
+  const data = []
+  const querySnapshot = await relaysCollection.limit(200).get()
+  querySnapshot.forEach(doc => data.push(doc.data()));
+  return data;
+}
+
+const getRelayDetails = async (fingerprint) => {
+  const doc = await relaysCollection.doc(fingerprint).get()
+  return doc.exists ? doc.data() : null;
+}
+
+const getRelaysByContry = async (countryCode) => {
+  const data = []
+  const querySnapshot = await relaysCollection.where('country', '==', countryCode).get()
+  querySnapshot.forEach(doc => data.push(doc.data()));
+  return data;
+}
+
+module.exports = {addRelay, getAllRelays, getRelayDetails, getRelaysByContry}
